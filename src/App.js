1d8fe8bd22735cebe3664d5258937e698bb094fd
adietyachristian <adietya.christian@ui.ac.id>
2023-03-03 12:56:44 +0700

    Create Homepage and Routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard.jsx';
+import Chat from './pages/Chat.jsx';
+import FindTutor from './pages/FindTutor.jsx';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Sidebar>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/find-tutor" element={<FindTutor />} />
+        </Routes>
+      </Sidebar>
+    </Router>
   );
-}
+};
 
 export default App;
