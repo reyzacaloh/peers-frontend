9d5e41e096c55b1134697f2b4c67ca65c3a767b4
Dixon Frederick <dixon.frederick@ui.ac.id>
2023-03-03 23:39:31 +0700

    [GREEN] Fix elements to build properly

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
             <h1>Profile page</h1>
             <div class="row">
                 <div class="col-md-3 border-right">
-                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"></img><br></br>
+                    <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img alt="profile" class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"></img><br></br>
                         <span class="font-weight-bold">someone</span><br></br>
                         <span class="text-black-50">something@mail.com</span><span> </span>
                     </div>    
