a71804085a5a84b27e585171461c279e89da93f2
ramadhan.azka <ramadhan.azka@ui.ac.id>
2023-03-03 21:32:23 +0700

    [RED] Create Login test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Login from "./components/Login";
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("Renders login form", () => {
+  render(<Login />);
+  const linkElement = screen.getByText(/E-mail/i);
+  const linkElement2 = screen.getByText(/Password/i);
+  expect(linkElement).toBeInTheDocument();
+  expect(linkElement2).toBeInTheDocument();
+})
