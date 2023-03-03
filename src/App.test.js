a2c7c83d1ffda6aa4ec4b08a123d40365ce9c925
joh_rakatn <johanes.raka@ui.ac.id>
2023-03-03 23:15:12 +0700

    [RED] fix missing import in test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import RegisterForm from './components/RegisterForm';
 
 test('renders learn react link', () => {
   render(<App />);
