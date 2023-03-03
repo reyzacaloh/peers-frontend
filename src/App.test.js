d7e6cb10f0b32b8825aaf695828a60b3c4cdd903
Dixon Frederick <dixon.frederick@ui.ac.id>
2023-03-03 23:18:31 +0700

    [RED] Create test for learner profile

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Profile from './pages/Profile';
 
 test('renders dashboard page', () => {
   render(<App />);
   const linkElement = screen.getByText(/dashboard page/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders Profile page', () => {
+  render(<Profile />);
+  const linkElement = screen.getByText(/Profile page/i);
+  expect(linkElement).toBeInTheDocument();
+}); 
