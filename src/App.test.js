14460e8ac98d3100a08f2038e94c90a562418b84
joh_rakatn <johanes.raka@ui.ac.id>
2023-03-03 23:04:29 +0700

    [RED] initialize render test for RegisterForm

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,19 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('all field in form fully renders', () => {
+  render(<RegisterForm />);
+  const emailField = screen.getByTestId("email");
+  const passwordField = screen.getByTestId("password");
+  const firstNameField = screen.getByTestId("first_name");
+  const lastNameField = screen.getByTestId("last_name");
+  const dateOfBirth = screen.getByTestId("date_of_birth");
+  const profilePic = screen.getByTestId("profile_picture");
+  expect(emailField).toBeInTheDocument();
+  expect(passwordField).toBeInTheDocument();
+  expect(firstNameField).toBeInTheDocument();
+  expect(lastNameField).toBeInTheDocument();
+  expect(dateOfBirth).toBeInTheDocument();
+  expect(profilePic).toBeInTheDocument();
+});
