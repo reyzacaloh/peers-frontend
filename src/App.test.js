e2370335fd0a885edd1d1870323660c3f9fcb1ac
adietyachristian <adietya.christian@ui.ac.id>
2023-03-04 11:01:12 +0700

    [RED] Create App and Auth Reducer Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,55 @@
-import { render, screen } from '@testing-library/react';
+import { render, renderHook, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import Sidebar from './components/Sidebar';
+import { BrowserRouter as Router } from "react-router-dom";
+import Chat from './pages/Chat';
+import FindTutor from './pages/FindTutor';
+import NotFound from './pages/NotFound';
+import React, { Component } from 'react';
+import Dashboard from './pages/Dashboard';
 
-test('renders dashboard page', () => {
+test('renders app', () => {
   render(<App />);
   const linkElement = screen.getByText(/dashboard page/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders dashboard page', () => {
+  render(<Dashboard />);
+  const linkElement = screen.getByText(/dashboard page/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders sidebar', () => {
+  render(
+    <Router>
+      <Sidebar />
+    </Router>
+  );
+  const linkElement = screen.getByText(/Pesan/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders chat', () => {
+  render(
+    <Chat />
+  );
+  const linkElement = screen.getByText(/Chat/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders sidebar', () => {
+  render(
+    <FindTutor />
+  );
+  const linkElement = screen.getByText(/Find Tutor/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders not found page', () => {
+  render(
+    <NotFound />
+  );
+  const linkElement = screen.getByText(/Not Found/i);
   expect(linkElement).toBeInTheDocument();
 });
