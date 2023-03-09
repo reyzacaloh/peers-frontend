7e922b17432e62e448d8aba53f8fe88336f29e9e
anisfarhan <fitsnaini@yahoo.co.id>
2023-03-09 23:59:08 +0700

    [GREEN] create search bar component

---
+++ src/components/search_bar/SearchBar.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div>
+        <input 
+            type="text" 
+            placeholder="Search..." 
+        />
+      </div>
+    )
+  }
+}
+
+export default SearchBar;
