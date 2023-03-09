123ef99887edc7c32eb02618cfa8c8f7a29f9db1
reyza.abyan <reyza.abyan@gmail.com>
2023-03-09 23:53:31 +0700

    [GREEN] PP-3 change button to navlink

--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FaPowerOff } from 'react-icons/fa';
 function Logout() {
 
     const handleLogout = () => {
+      
       localStorage.removeItem("jwtToken");
+      
     };
   
     return (
-      <button onClick={handleLogout}>Logout</button>
+      <NavLink 
+      className="link"
+      activeclassname="active"
+      onClick={handleLogout} 
+      >
+        <div className="icon"><FaPowerOff/></div>
+        <div className="link_text">Logout</div>
+    </NavLink>
     );
   }
   
-  export default Logout;
+export default Logout;
