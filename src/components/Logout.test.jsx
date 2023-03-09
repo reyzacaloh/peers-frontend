3b67e473ee6a1ef36e9ac411d60deb2dddeb423a
reyza.abyan <reyza.abyan@gmail.com>
2023-03-09 23:51:51 +0700

    [RED] fix test

--- src/components/Logout.test.jsx
+++ src/components/Logout.test.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import Logout from './Logout';
 
-describe('Logout component', () => {
-  test('should remove JWT token from local storage on click', () => {
-    const jwtToken = 'sampleJWTToken';
-    localStorage.setItem('jwtToken', jwtToken);
+describe('Logout', () => {
+  test('renders a "Logout" button', () => {
+    render(
+      <BrowserRouter>
+        <Logout />
+      </BrowserRouter>
+    );
 
-    render(<Logout />);
+    const logoutButton = screen.getByText(/Logout/i);
+    expect(logoutButton).toBeInTheDocument();
+  });
 
-    const logoutButton = screen.getByRole('button', { name: /logout/i });
+  test('calls the handleLogout function when clicked', () => {
+    const handleLogout = jest.fn();
+    render(
+      <BrowserRouter>
+        <Logout handleLogout={handleLogout} />
+      </BrowserRouter>
+    );
 
+    const logoutButton = screen.getByText(/Logout/i);
     fireEvent.click(logoutButton);
 
+    expect(handleLogout).toHaveBeenCalledTimes(1);
     expect(localStorage.getItem('jwtToken')).toBeNull();
   });
 });
