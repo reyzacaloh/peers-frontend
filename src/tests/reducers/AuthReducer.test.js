5fd845f7ded3ff8dc8657ea0f4872a837bc091f1
adietyachristian <adietya.christian@ui.ac.id>
2023-03-12 01:46:45 +0700

    [RED] Create Auth Context Test

--- src/tests/reducers/AuthReducer.test.js
+++ src/tests/reducers/AuthReducer.test.js
@@ -4,7 +4,7 @@ import { authReducer, initialState } from '../../reducers/AuthReducer';
 it('returns autheticated for successful login', () => {
   const updateAction = {type: 'LOGIN', payload: initialState };
   const updatedState = authReducer(initialState, updateAction);
-  expect(updatedState).toEqual({isAuthenticated: true, user: null, token: null});
+  expect(updatedState).toEqual({isAuthenticated: true, user: null, token: null, isTutor: false});
 });
 
 it('returns not autheticated for unsuccessful login', () => {
