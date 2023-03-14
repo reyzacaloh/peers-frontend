c9254e6bce87953274ab2072e22b27eb3a657c9d
Ramadhan Azka Dhamawan <ramadhan.azka@ui.ac.id>
2023-03-14 23:12:39 +0700

    PBI 2 create login form - update post variable

--- src/components/loginForm/LoginForm.jsx
+++ src/components/loginForm/LoginForm.jsx
@@ -21,7 +21,7 @@ const LoginForm = () => {
             let response = await axios.post(
                 `${host}/api/auth/token`,
                 {
-                    username: values.email,
+                    email: values.email,
                     password: values.pass
                 }
             );
