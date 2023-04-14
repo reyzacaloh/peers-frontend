3c75c872fd71b7f34508e5acdcea53d5f917b20d
Hakim Amarullah <hakim.amarullah@ui.ac.id>
2023-04-14 10:33:09 +0700

    PBI 7 Chat Page

--- src/routes/AuthRoutes.jsx
+++ src/routes/AuthRoutes.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Sidebar from '../components/Sidebar.jsx';
-import Chat from "../pages/Chat.jsx";
+import Sidebar from "../components/Sidebar.jsx";
+import Chat from "../pages/Chat/Chat.jsx";
 import FindTutor from "../pages/FindTutor.jsx";
 import NotFound from "../pages/NotFound";
 import TutorDashboard from "../pages/TutorDashboard";
@@ -13,21 +13,15 @@ import TutorDetail from '../pages/TutorDetail/TutorDetail.jsx';
 import { AuthContext } from "../contexts/AuthContext";
 
 function AuthRoutes() {
-
     const { state } = React.useContext(AuthContext);
-    
-    return (
 
+    return (
         <Routes>
             <Route path="/" element={
                 <Sidebar>
                     <FindTutor />
                 </Sidebar>} />
-            <Route
-                path="/chat" element={
-                    <Sidebar>
-                        <Chat />
-                    </Sidebar>} />
+            <Route path="/chat" element={<Chat />} />
             <Route
                 path="/profile" element={
                     <Sidebar>
@@ -36,14 +30,14 @@ function AuthRoutes() {
             <Route
                 path="/verify" element={
                     <Sidebar>
-                        <Verification/>
+                        <Verification />
                     </Sidebar>} />
             <Route
                 path="/schedule" element={
-                <Sidebar>
-                    <TutorScheduleForm/>
-                </Sidebar>} />
-            
+                    <Sidebar>
+                        <TutorScheduleForm />
+                    </Sidebar>} />
+
             <Route path="/tutor" element={
                 !state.isTutor ?
                     <RegisterTutorForm /> :
@@ -52,7 +46,7 @@ function AuthRoutes() {
                     </Sidebar>
             } />
             <Route path="/tutor/:id" element={
-                    <TutorDetail />
+                <TutorDetail />
             } />
             <Route path="*" element={<NotFound />} />
         </Routes>
