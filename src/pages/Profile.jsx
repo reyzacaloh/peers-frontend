8eae2ba2c178faad34b1bc79a0b93e52db002bbf
Dixon Frederick <dixon.frederick@ui.ac.id>
2023-04-27 21:38:52 +0700

    [REFACTOR] Remove dummy data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -31,15 +31,15 @@ const Profile = () => {
       <div className="grid-container">
         <div className="card">
           <div>
-            <img className="rounded" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="profile"/>
+            <img className="rounded" src={profile.profile_picture} alt="profile"/>
           </div>
           <div className="content">
             <h1>
-              Nama{profile.first_name} {profile.last_name}{' '}
+              {profile.first_name} {profile.last_name}{' '}
             </h1>
             <br></br>
-            <h4><MdEmail></MdEmail> &nbsp;&nbsp;someone@email.com{profile.email}</h4>
-            <h4><MdDateRange></MdDateRange> &nbsp;&nbsp;10-10-2010{profile.date_of_birth}</h4>
+            <h4><MdEmail></MdEmail> &nbsp;&nbsp;{profile.email}</h4>
+            <h4><MdDateRange></MdDateRange> &nbsp;&nbsp;{profile.date_of_birth}</h4>
           </div>
         </div>
         <div className="content">
