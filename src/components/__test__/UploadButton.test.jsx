fb44f9969374ea4af3279219164521789379c9d3
Dixon Frederick <dixon.frederick@ui.ac.id>
2023-05-01 19:04:51 +0700

    [REFACTOR] Update some tests

--- src/components/__test__/UploadButton.test.jsx
+++ src/components/__test__/UploadButton.test.jsx
@@ -1,30 +1,41 @@
 import React from "react";
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, act } from '@testing-library/react';
 import UploadButton from "../chat/UploadButton.jsx";
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import AuthContextProvider from '../../contexts/AuthContext';
 
 describe('UploadButton', () => {
+  let file;
+  let input;
+  file = new File(['test'], 'test.png', { type: 'image/png' });
+  beforeEach(() => {
+    render(<AuthContextProvider>
+      <UploadButton />
+    </AuthContextProvider>
+      , { wrapper: BrowserRouter });
+    input = screen.getByTestId('file-browser-input');
+  });
+
     it('should add a file on file load', () => {
-      render(<UploadButton />);
-      const file = new File(['test'], 'test.png', { type: 'image/png' });
-      const input = screen.getByTestId('file-browser-input');
       fireEvent.change(input, { target: { files: [file] } });
       expect(input).toBeInTheDocument();
     });
   
     it('should remove a file on click of remove button', () => {
-      render(<UploadButton />);
-      const file = new File(['test'], 'test.png', { type: 'image/png' });
-      const input = screen.getByTestId('file-browser-input');
       fireEvent.change(input, { target: { files: [file] } });
       const img = screen.queryByTestId('image');
       expect(img).not.toBeInTheDocument();
     });
-  
+
+    it('allows uploading of an image', () => {
+      fireEvent.change(input, { target: { files: [file] } });
+      expect(input.src).toContain('');
+    });
+
     it('should remove all files on click of Upload button', () => {
-      render(<UploadButton />);
       const file1 = new File(['test'], 'test1.png', { type: 'image/png' });
       const file2 = new File(['test'], 'test2.png', { type: 'image/png' });
-      const input = screen.getByTestId('file-browser-input');
       fireEvent.change(input, { target: { files: [file1, file2] } });
       const uploadBtn = screen.getByRole('button', { name: 'Upload' });
       fireEvent.click(uploadBtn);
