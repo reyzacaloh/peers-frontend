4bb6d189fe29b32277fd630b0bc91d09dd696b73
reyza.abyan <reyza.abyan@gmail.com>
2023-05-07 17:18:38 +0700

    [GREEN] fix some bug

---
+++ src/components/__test__/Logout.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Logout from '../logout/Logout';
+import AuthContextProvider from "../../contexts/AuthContext";
+
+describe('Logout', () => {
+  test('renders a "Logout" button', () => {
+    render(
+      <AuthContextProvider>
+        <BrowserRouter>
+          <Logout />
+        </BrowserRouter>
+      </AuthContextProvider>
+      
+    );
+
+    const logoutButton = screen.getByTestId("logout");
+    expect(logoutButton).toBeInTheDocument();
+  });
+
+  test('calls the handleLogout function when clicked', () => {
+    const handleLogout = jest.fn();
+    render(
+      <AuthContextProvider>
+        <BrowserRouter>
+          <Logout handleLogout={handleLogout} />
+        </BrowserRouter>
+      </AuthContextProvider>
+      
+    );
+
+    const logoutButton = screen.getByTestId("logout");
+    fireEvent.click(logoutButton);
+
+    expect(localStorage.getItem('jwtToken')).toBeNull();
+  });
+});
