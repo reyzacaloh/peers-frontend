c3bcaa891e92290f90b8ceac3d2022c4ed8af21b
Dixon Frederick <dixon.frederick@ui.ac.id>
2023-05-11 18:44:50 +0700

    [RED] Creating tests for Rating component

---
+++ src/components/__test__/Rating.test.jsx
@@ -0,0 +1,37 @@
+import { render, fireEvent } from '@testing-library/react';
+import Rating from '../rating';
+
+describe('Rating component', () => {
+  test('renders the component', () => {
+    render(<Rating />);
+  });
+
+  test('handles click event on star rating', () => {
+    const { container } = render(<Rating />);
+    const stars = container.querySelectorAll('svg');
+
+    fireEvent.click(stars[2]); 
+  });
+
+  test('handles mouse over event on star rating', () => {
+    const { container } = render(<Rating />);
+    const stars = container.querySelectorAll('svg');
+
+    fireEvent.mouseOver(stars[4]); 
+  });
+
+  test('handles mouse leave event on star rating', () => {
+    const { container } = render(<Rating />);
+    const stars = container.querySelectorAll('svg');
+
+    fireEvent.mouseOver(stars[2]); 
+    fireEvent.mouseLeave(stars[2]); 
+  });
+
+  test('handles submit button click', () => {
+    const { container } = render(<Rating />);
+    const submitButton = container.querySelector('button');
+
+    fireEvent.click(submitButton);
+  });
+});
