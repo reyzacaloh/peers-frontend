c47eabd3a7d05f7f4c15a528e192b6327216854b
reyza.abyan <reyza.abyan@gmail.com>
2023-05-22 05:48:24 +0700

    [REFACTOR] restructure getcontact & getchats func

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -6,9 +6,13 @@ import { ChatPartnerContext } from "../../contexts/ChatPartnerContext";
 import {
   doc,
   onSnapshot,
+  getDoc,
+  setDoc,
+  updateDoc,
+  serverTimestamp,
 } from "firebase/firestore";
 import { db } from "../../firebase";
-
+import axios from "axios";
 
 const Chat = () => {
   const [open, setOpen] = useState(false);
@@ -18,20 +22,111 @@ const Chat = () => {
   const [chats, setChats] = useState([]);
   const { currentUser } = React.useContext(ChatContext);
   const { dispatch } = React.useContext(ChatPartnerContext);
-  
 
   useEffect(() => {
-    const getChats = () => {
-      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (dok) => {
-        setChats(dok.data());
-      });
-
-      return () => {
-        unsub();
-      };
+    const handlePromiseRejection = (event) => {
+      console.log("Uncaught Promise Rejection:", event.reason);
+      window.location.reload();
     };
+
+    window.addEventListener("unhandledrejection", handlePromiseRejection);
+
+    const getChats = async () => {
+      try {
+        const unsub = onSnapshot(
+          doc(db, "userChats", currentUser.uid),
+          (dok) => {
+            if (dok.exists()) {
+              setChats(dok.data());
+            } else {
+              console.log("No chats data available.");
+            }
+          }
+        );
+
+        return () => {
+          unsub();
+        };
+      } catch (error) {
+        console.log("Error getting chats:", error);
+      }
+    };
+    const getContacts = async () => {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/booking/booking-paid`,
+          {
+            headers: {
+              authorization: `Bearer ${JSON.parse(
+                localStorage.getItem("token")
+              )}`,
+            },
+          }
+        );
+        const book_list = response.data.booking_list;
+        book_list.forEach(async (item) => {
+          const combinedId =
+            currentUser.uid > item.tutor_uid
+              ? currentUser.uid + item.tutor_uid
+              : item.tutor_uid + currentUser.uid;
+
+          const learner_check = await getDoc(
+            doc(db, "userChats", currentUser.uid)
+          );
+          if (!learner_check.exists()) {
+            await setDoc(doc(db, "userChats", currentUser.uid), {});
+          }
+
+          const tutor_check = await getDoc(
+            doc(db, "userChats", item.tutor_uid)
+          );
+          if (!tutor_check.exists()) {
+            await setDoc(doc(db, "userChats", item.tutor_uid), {});
+          }
+
+          const res = await getDoc(doc(db, "chats", combinedId));
+
+          if (!res.exists()) {
+            //create a chat in chats collection
+            await setDoc(doc(db, "chats", combinedId), { messages: [] });
+
+            //create user chats
+            await updateDoc(doc(db, "userChats", currentUser.uid), {
+              [combinedId + ".userInfo"]: {
+                latest_message: "",
+                profile_pic: item.profile_pic,
+                uid: item.tutor_uid,
+                username: item.tutor_name,
+              },
+              [combinedId + ".date"]: serverTimestamp(),
+            });
+
+            await updateDoc(doc(db, "userChats", item.tutor_uid), {
+              [combinedId + ".userInfo"]: {
+                latest_message: "",
+                profile_pic: currentUser.profile_picture,
+                uid: currentUser.uid,
+                username: currentUser.first_name + " " + currentUser.last_name,
+              },
+              [combinedId + ".date"]: serverTimestamp(),
+            });
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getContacts();
     currentUser.uid && getChats();
-  }, [currentUser.uid]);
+    return () => {
+      window.removeEventListener("unhandledrejection", handlePromiseRejection);
+    };
+  }, [
+    currentUser.first_name,
+    currentUser.last_name,
+    currentUser.profile_picture,
+    currentUser.uid,
+  ]);
 
   const handleData = (allChat) => {
     try {
@@ -39,7 +134,6 @@ const Chat = () => {
         (a, b) => b[1].date - a[1].date
       );
       return sortedChat.map((map) => map[1].userInfo);
-      
     } catch (error) {
       return [];
     }
