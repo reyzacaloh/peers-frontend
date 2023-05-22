0fc9e6c81b8409762374e86ef6a5b8f4fbf10ee9
Dixon Frederick <dixon.frederick@ui.ac.id>
2023-05-22 19:12:30 +0700

    [REFACTOR] Fixing tests

--- src/components/__test__/Rating.test.jsx
+++ src/components/__test__/Rating.test.jsx
@@ -1,37 +1,121 @@
-import { render, fireEvent } from '@testing-library/react';
+/* eslint-disable testing-library/no-unnecessary-act */
+import { render, screen, act, waitFor, fireEvent } from '@testing-library/react';
+import axios from 'axios';
+import AuthContextProvider from '../../contexts/AuthContext';
+import userEvent from '@testing-library/user-event'
 import Rating from '../Rating';
+import "@testing-library/jest-dom/extend-expect";
+import { BrowserRouter, useNavigate } from 'react-router-dom';
+import Popup from 'reactjs-popup';
 
-describe('Rating component', () => {
-  test('renders the component', () => {
-    render(<Rating />);
+describe('Rating test', () => {
+  const mockUseNavigate = jest.fn();
+
+  beforeEach(() => {
+      jest.mock('react-router-dom', () => ({
+        ...jest.requireActual('react-router-dom'),
+        useNavigate: () => mockUseNavigate,
+      }));
   });
 
-  test('handles click event on star rating', () => {
-    const { container } = render(<Rating />);
-    const stars = container.querySelectorAll('svg');
+  test('should call useNavigate with the correct path on form submission', async () => {
+    
+    const mockPost = jest.spyOn(axios, 'post');
+    mockPost.mockResolvedValueOnce({});
+    const { getByText } = render(<AuthContextProvider>
+      <Rating tutorId={1} />
+    </AuthContextProvider>
+      , { wrapper: BrowserRouter });
+    fireEvent.click(getByText('Rate Tutor'));
+    fireEvent.click(screen.getByText('Save'));
 
-    fireEvent.click(stars[2]); 
+    
+    await waitFor(() => expect(mockPost).toHaveBeenCalledWith(
+      `${process.env.REACT_APP_API_URL}/api/tutor_form/rate/`,
+      { rating: '0' },
+      {
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
+        },
+      }
+    ));
   });
 
-  test('handles mouse over event on star rating', () => {
-    const { container } = render(<Rating />);
-    const stars = container.querySelectorAll('svg');
+  test('should handle form submission error', async () => {
+    const mockPost = jest.spyOn(axios, 'post');
+    const errorResponse = {
+      code: '500',
+    };
+    mockPost.mockRejectedValueOnce(errorResponse);
+    const { getByText } = render(<AuthContextProvider>
+      <Rating tutorId={1} />
+    </AuthContextProvider>
+      , { wrapper: BrowserRouter });
+    fireEvent.click(getByText('Rate Tutor'));
+    fireEvent.click(screen.getByText('Save'));
 
-    fireEvent.mouseOver(stars[4]); 
+    await waitFor(() => expect(mockPost).toHaveBeenCalledWith(
+      `${process.env.REACT_APP_API_URL}/api/tutor_form/rate/`,
+      { rating: '0' },
+      {
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
+        },
+      }
+    ));
   });
 
-  test('handles mouse leave event on star rating', () => {
-    const { container } = render(<Rating />);
-    const stars = container.querySelectorAll('svg');
+  it('should change background color on MouseOver and MouseOut events', () => {
+    const { getByText } = render(<AuthContextProvider>
+      <Rating tutorId={1} />
+    </AuthContextProvider>
+      , { wrapper: BrowserRouter });
+    fireEvent.click(getByText('Rate Tutor'));
+    const closeButton = screen.getByText('[x]');
 
-    fireEvent.mouseOver(stars[2]); 
-    fireEvent.mouseLeave(stars[2]); 
+    fireEvent.mouseOver(closeButton);
+    expect(closeButton).toHaveStyle({background: 'grey'});
+    fireEvent.mouseOut(closeButton);
+    expect(closeButton).not.toHaveStyle({background: 'grey'});
   });
 
-  test('handles submit button click', () => {
-    const { container } = render(<Rating />);
-    const submitButton = container.querySelector('button');
+  it('should call close function on Popup close', () => {
+    const { getByText } = render(<AuthContextProvider>
+      <Rating tutorId={1} />
+    </AuthContextProvider>
+      , { wrapper: BrowserRouter });
+    fireEvent.click(getByText('Rate Tutor'));
+    const closeButton = screen.getByText('[x]');
+    const mockClose = jest.fn();
+
+    fireEvent.click(closeButton);
+  });
+
+  test('should submit the form on form submission', () => {
+    const { getByText } = render(<AuthContextProvider>
+      <Rating tutorId={1} />
+    </AuthContextProvider>
+      , { wrapper: BrowserRouter });
+    fireEvent.click(getByText('Rate Tutor'));
+    const submitButton = screen.getByText('Save');
+    const mockSubmit = jest.fn();
+
+    fireEvent.submit(submitButton);
+  });
+
+  test('should disable the button while submitting', () => {
+    const { getByText } = render(<AuthContextProvider>
+      <Rating tutorId={1} />
+    </AuthContextProvider>
+      , { wrapper: BrowserRouter });
+    fireEvent.click(getByText('Rate Tutor'));
+    const submitButton = screen.getByText('Save');
 
     fireEvent.click(submitButton);
+
+    expect(submitButton).toBeDisabled();
   });
+
 });
