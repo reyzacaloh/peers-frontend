c95b67622cee0052a7dbbf734a5879d0bd7d6f8d
Dixon Frederick <dixon.frederick@ui.ac.id>
2023-05-25 22:25:52 +0700

    Pbi 14 learner profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Card } from 'antd';
 import './Profile.css';
 import axios from 'axios';
 import LearnerSchedule from '../components/LearnerSchedule';
@@ -31,17 +32,25 @@ const Profile = () => {
       <h1>Profile page</h1>
       <div className="grid-container">
         <div className="card">
+        <Card
+            title={'Welcome, ' + profile.first_name}
+            style={{
+            width: 500,
+            height: 1000,
+          }}
+          headStyle={{fontSize: 25, color:'white', backgroundColor: '#279686', border: '1.5px solid black' }}
+          bodyStyle={{border: '1.5px solid black'}}
+        >
           <div>
             <img className="profile-usr" src={profile.profile_picture} alt="profile"/>
           </div>
-          <div className="content">
-            <h1>
+            <h1 className="card_h1">
               {profile.first_name} {profile.last_name}{' '}
             </h1>
             <br></br>
-            <h4><MdEmail></MdEmail> &nbsp;&nbsp;{profile.email}</h4>
-            <h4><MdDateRange></MdDateRange> &nbsp;&nbsp;{profile.date_of_birth}</h4>
-          </div>
+            <h4 className="card_h4"><MdEmail></MdEmail> &nbsp;&nbsp;{profile.email}</h4>
+            <h4 className="card_h4"><MdDateRange></MdDateRange> &nbsp;&nbsp;{profile.date_of_birth}</h4>
+        </Card>
         </div>
         <div className="content">
           <LearnerSchedule/>
